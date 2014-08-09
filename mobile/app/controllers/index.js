0280928a20dc22eae9a8e0fed8685f0f08edd8ea
Matthew Congrove <mattcongrove@gmail.com>
2014-08-09 16:56:46 -0500

    Mobile: Adding Core.js

--- mobile/app/controllers/index.js
+++ mobile/app/controllers/index.js
@@ -1,5 +1,21 @@
-function doClick(e) {
-    alert($.label.text);
+/**
+ * @class Controllers.index
+ */
+
+// App bootstrap
+var App = require("core");
+
+// Make sure we always have a reference to the navigation window for iOS
+if(OS_IOS) {
+	App.navigationWindow = $.navWindow;
+	App.navigationWindow.open();
+} else {
+	$.indexWindow.open();
 }
 
-$.index.open();
+// Init our app singleton
+App.init();
+
+$.label.addEventListener("click", function() {
+	App.Navigator.open("screen");
+});
