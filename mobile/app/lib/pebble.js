2c244717e5c56c6df1ca970041d0db16d4a9c736
Matthew Congrove <mattcongrove@gmail.com>
2014-08-18 14:38:17 -0500

    Mobile: Improving connection to Pebble

--- mobile/app/lib/pebble.js
+++ mobile/app/lib/pebble.js
@@ -5,62 +5,85 @@ Pebble.setAppUUID("226834ae-786e-4302-a52f-6e7efc9f990b");
 exports.connected = false;
 
 exports.connect = function(_data) {
-	Pebble.getVersionInfo({
-		success: function(_event) {
-			watchConnected();
-			launchApp(_data ? _data : null);
-		},
-		error: function(_event) {
-		   watchDisonnected();
-		   
-		   _data.error ? _data.error() : null;
-		}
-	});
+	Ti.API.info("@Pebble connect");
+	
+	launchApp(_data ? _data : null);
 };
 
 exports.sendMessage = function(_data) {
+	Ti.API.info("@Pebble sendMessage");
+	
 	if(exports.connected) {
 		Pebble.sendMessage({
 			message: _data.message,
-			success: _data.success ? _data.success : null,
-			error: _data.error ? _data.error : null
+			success: function() {
+				Ti.API.info("@Pebble sendMessage:success");
+				
+				_data.success ? _data.success() : null;
+			},
+			error: function() {
+				Ti.API.info("@Pebble sendMessage:error");
+				
+				_data.error ? _data.error() : null;
+			}
 		});
 	}
 };
 
 function watchConnected(_event) {
+	Ti.API.info("@Pebble watchConnected");
+	
 	exports.connected = true;
 }
 
 function watchDisonnected(_event) {
+	Ti.API.info("@Pebble watchDisonnected");
+	
 	exports.connected = false;
 }
 
 function launchApp(_data) {
+	Ti.API.info("@Pebble launchApp");
+	
+	Pebble.launchApp({
+		success: function() {
+			Ti.API.info("@Pebble launchApp:success");
+			
+			watchConnected();
+			
+			_data.success ? _data.success() : null;
+		},
+		error: function() {
+			Ti.API.info("@Pebble launchApp:error");
+			
+			watchDisconnected();
+			
+			_data.error ? _data.error() : null;
+		}
+	});
+}
+
+function killApp(_data) {
+	Ti.API.info("@Pebble killApp");
+	
 	if(exports.connected) {
-		Pebble.launchApp({
-			success: function() {},
+		Pebble.killApp({
+			success: function() {
+				Ti.API.info("@Pebble killApp:success");
+			},
 			error: function() {
-				_data.error ? _data.error : null;
+				Ti.API.info("@Pebble killApp:error");
 				
 				watchDisconnected();
 			}
 		});
-		
-		_data.success ? _data.success() : null;
 	}
 }
 
-function killApp(_data) {
-	if(exports.connected) {
-		Pebble.killApp({
-			success: function() {},
-			error: function() {
-				watchDisconnected();
-			}
-		});
-	}
+function receiveMessage(_data) {
+	Ti.API.warn(JSON.stringify(_data));
 }
 
 Pebble.addEventListener("watchConnected", watchConnected);
 Pebble.addEventListener("watchDisconnected", watchDisonnected);
+Pebble.addEventListener("update", receiveMessage);
